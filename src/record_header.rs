23edaa5eff18c0dd3b88d5807f2b19ef3f8c350e
r9n9ga7 <r9n9ga7@gmail.com>
2026-02-10 17:37:53 +0900

    feat: add reserved bytes for record header

--- src/record_header.rs
+++ src/record_header.rs
@@ -2,11 +2,12 @@ pub struct RecordHeader {
     is_active: bool,
     content_size: u64,
     id: u64,
+    reserved: [u8; 32],
 }
 
 impl RecordHeader {
     pub fn new(content_size: u64, id: u64) -> Self {
-        Self { is_active: true, content_size, id }
+        Self { is_active: true, content_size, id, reserved: [0; 32] }
     }
 
     pub fn from_bytes(bytes: &[u8]) -> Self {
@@ -14,11 +15,13 @@ impl RecordHeader {
         let is_active = bytes[0] != 0;
         let content_size = u64::from_le_bytes(bytes[1..9].try_into().unwrap());
         let id = u64::from_le_bytes(bytes[9..17].try_into().unwrap());
+        let reserved = bytes[17..49].try_into().unwrap();
 
         Self {
             is_active,
             content_size,
             id,
+            reserved,
         }
     }
 
@@ -28,6 +31,7 @@ impl RecordHeader {
         buffer.extend_from_slice(&[self.is_active as u8]);
         buffer.extend_from_slice(&self.content_size.to_le_bytes());
         buffer.extend_from_slice(&self.id.to_le_bytes());
+        buffer.extend_from_slice(&self.reserved);
 
         buffer
     }
@@ -35,5 +39,5 @@ impl RecordHeader {
     pub fn get_content_size(&self) -> u64 { self.content_size }
     pub fn get_id(&self) -> u64 { self.id }
 
-    pub const fn size() -> usize { 2 * size_of::<u64>() + 1 }
+    pub const fn size() -> usize { 2 * size_of::<u64>() + 1 + 32 }
 }
