ef79b3c4cb694194b7a9e8c1303854c2827a13cd
r9n9ga7 <r9n9ga7@gmail.com>
2026-02-14 00:35:56 +0900

    test: add tests for Config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum ConfigError {
     StorageNotFound,
     StorageAlreadyExists,
@@ -59,5 +60,62 @@ impl Config {
 
     pub fn has_storages(&self) -> bool {
         !self.storage_list.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_config_has_no_storages_and_no_active() {
+        let config = Config::new();
+
+        assert!(!config.has_storages());
+        assert_eq!(config.get_active_storage(), None)
+    }
+
+    #[test]
+    fn add_storage_works() {
+        let mut config = Config::new();
+
+        assert!(config.add_storage("test").is_ok());
+        assert_eq!(config.get_storage_list(), &["test".to_string()]);
+    }
+
+    #[test]
+    fn add_storage_duplicate_fails() {
+        let mut config = Config::new();
+        config.add_storage("test").unwrap();
+
+        assert_eq!(
+            config.add_storage("test"),
+            Err(ConfigError::StorageAlreadyExists)
+        );
+    }
+
+    #[test]
+    fn set_active_storage_requires_existing_storage() {
+        let mut config = Config::new();
+        assert_eq!(
+            config.set_active_storage("test"),
+            Err(ConfigError::StorageNotFound)
+        );
+    }
+
+    #[test]
+    fn set_active_storage_works() {
+        let mut config = Config::new();
+        config.add_storage("test").unwrap();
+        config.set_active_storage("test").unwrap();
+
+        assert_eq!(config.get_active_storage(), Some("test".to_string()));
+    }
+
+    #[test]
+    fn set_storage_list_rejects_duplicates() {
+        let mut config = Config::new();
+        let result = config.set_storage_list(vec!["test".to_string(), "test".to_string()]);
+        assert!(result.is_err());
     }
 }
